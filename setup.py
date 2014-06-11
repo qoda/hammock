a848bf2769bd0618caef1a624129c7ee291fee6b
qoda <jonathan.bydendyk@is.co.za>
2014-06-11 10:14:09 +0200

    fixed dependancies

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='hammock',
     py_modules=['hammock'],
-    version='0.2.4',
+    version='0.3.0',
     description='rest like a boss',
     author='Kadir Pekel',
     author_email='kadirpekel@gmail.com',
     url='https://github.com/kadirpekel/hammock',
     install_requires=[
-        'requests>=1.1.0',
+        'requests>=2.3.0',
         'vcrpy==1.0.2'
     ],
     tests_require=['httpretty==0.5.4'],
